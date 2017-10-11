80e35efc45ef6942b815bc54c3713ba73b9d2ea2
Sajil Shakya <shakya.sajil@gmail.com>
2017-10-11 10:37:36 -0400

    added CRUD functionality

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,50 @@
 class ItemsController < ApplicationController
+  before_action :find_item, only: [:show, :edit, :update, :destroy]
   def index
     @items = Item.all
     render :json => @items
   end
 
+  def show
+
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new()
+
+    if @play.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :category, :price, :quantity, :description)
+  end
+
+  def find_item
+    @item = Item.find(params[:id])
+  end
 end
